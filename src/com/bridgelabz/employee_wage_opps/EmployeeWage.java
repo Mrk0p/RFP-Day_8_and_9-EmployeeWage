6eca9e4cdd21766f826ac3baa8caa09812b2d442
Mrk0p <sachinwele23@gmail.com>
2023-01-18 18:36:31 +0530

    Added : Monthly wage logic

--- src/com/bridgelabz/employee_wage_opps/EmployeeWage.java
+++ src/com/bridgelabz/employee_wage_opps/EmployeeWage.java
@@ -13,26 +13,37 @@ public class EmployeeWage {
         int fullDayHours = 8;
         int halfDayHours = 4;
         int dailyWage = 0;
-        int randomNum = random.nextInt(2);
-        switch (randomNum) {
+        int monthlyWage = 0;
+        int workingDaysPerMonth = 20;
 
-            case 0:
-                System.out.println("Employee is Absent");
-                System.out.println("Monthly wage will be: ");
-                break;
-            case 1:
-                System.out.println("Employee is Present");
-                System.out.println("Monthly wage for Full Day: ");
-                dailyWage = wagePerHour * fullDayHours;
-                break;
-            case 2:
-                System.out.println("Employee is Present");
-                System.out.println("Monthly wage for Half Day: ");
-                dailyWage = wagePerHour * halfDayHours;
-                break;
+        for (int i = 1; i <= workingDaysPerMonth; i++) {
+            int randomNum = random.nextInt(3);
+
+
+            switch (randomNum) {
+
+                case 0:
+                    System.out.println("Employee is Absent");
+                    System.out.println("Monthly Salary will be: ");
+                    break;
+                case 1:
+                    System.out.println("Employee is Present");
+                    System.out.println("Monthly salary for Full Day: ");
+                    dailyWage = wagePerHour * fullDayHours;
+                    break;
+                case 2:
+                    System.out.println("Employee is Present");
+                    System.out.println("Monthly Salary for Half Day: ");
+                    dailyWage = wagePerHour * halfDayHours;
+                    break;
+
+            }
+            monthlyWage = monthlyWage + dailyWage;
+            System.out.println(+dailyWage);
 
         }
-        System.out.println(+dailyWage);
+        System.out.println("Monthly Salary: " + monthlyWage);
 
     }
+
 }
