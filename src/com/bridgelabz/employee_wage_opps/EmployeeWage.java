6ac35c6166782cdba17a462cba954a2a61efb9f8
Mrk0p <sachinwele23@gmail.com>
2023-01-18 18:28:30 +0530

    Added : Full Time logic

--- src/com/bridgelabz/employee_wage_opps/EmployeeWage.java
+++ src/com/bridgelabz/employee_wage_opps/EmployeeWage.java
@@ -9,11 +9,17 @@ public class EmployeeWage {
         System.out.println("Welcome to Employee Wage Computation Program");
 
         Random random = new Random();
+        int wagePerHour = 20;
+        int fullDayHours = 8;
+        int dailyWage = 0;
         int randomNum = random.nextInt(2);
 
-        if (randomNum == 1)
+        if (randomNum == 1) {
             System.out.println("Employee  is Present");
-        else
+            dailyWage = wagePerHour * fullDayHours;
+        } else
             System.out.println("Employee  is Absent");
+        System.out.println("salary is: " + dailyWage);
+
     }
 }
